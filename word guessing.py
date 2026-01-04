f485ccb92657c1b9aeb741acf5a1d558de24c4dc
shagun-saini <shagunsaini205@gmail.com>
2026-01-04 23:38:57 +0530

    Add the methods

--- word guessing.py
+++ word guessing.py
@@ -1,16 +1,15 @@
 #Word guessing game project.
 import random
-while True:
 
- words=["Articles","World","Fruits","Vegetables","Antartic","Ameliorate",
+words=["Articles","World","Fruits","Vegetables","Antartic","Ameliorate",
        "Finding","Dream","Developer","Particular","Searching","Individual"]
 
- guessing_game=random.choice(words)
+guessing_game=random.choice(words)
+while True:
+  guesing_word=input("Write the guessing word")
 
- guesing_word=input("Write the guessing word")
-
- if(guessing_game==words):
+  if guesing_word==guessing_game:
     print("Congratulation, your guessing word is correct")
     break
- else:
-    print("Sorry")
+  else:
+    print("Sorry") 
