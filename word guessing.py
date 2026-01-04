f2682f129d1a31d526bceb93516cbd4c3a6344c1
shagun-saini <shagunsaini205@gmail.com>
2026-01-04 23:55:44 +0530

    Updated logic and corrected spellings

--- word guessing.py
+++ word guessing.py
@@ -4,12 +4,15 @@ import random
 words=["Articles","World","Fruits","Vegetables","Antartic","Ameliorate",
        "Finding","Dream","Developer","Particular","Searching","Individual"]
 
-guessing_game=random.choice(words).upper()
+guessing_game=random.choice(words)
+Failed=0
 while True:
-  guesing_word=input("Write the guessing word").strip().upper()
+  guesing_word=input("Write the guessing word").strip()
 
   if guesing_word==guessing_game:
     print("Congratulation, your guessing word is correct")
+    Failed+=1
+    print("Failed",Failed)
     break
   else:
     print("Sorry") 
