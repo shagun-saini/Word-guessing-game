48cf5a6294401cdc7e9240d3ceaf2727d053fc14
shagun-saini <shagunsaini205@gmail.com>
2026-01-04 23:14:44 +0530

    Add the loop line

---
+++ word guessing.py
@@ -0,0 +1,14 @@
+#Word guessing game project.
+import random
+
+words=["Articles","World","Fruits","Vegetables","Antartic","Ameliorate",
+       "Finding","Dream","Developer","Particular","Searching","Individual"]
+
+guessing_game=random.choice(words)
+
+guesing_word=input("Write the guessing word")
+
+if(guessing_game==words):
+    print("Congratulation, your guessing word is correct")
+else:
+    print("Sorry")
