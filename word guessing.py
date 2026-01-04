aa2c777f9ee31581a0358a58c14424ad7567021d
shagun-saini <shagunsaini205@gmail.com>
2026-01-04 23:42:24 +0530

    Updated method

--- word guessing.py
+++ word guessing.py
@@ -4,9 +4,9 @@ import random
 words=["Articles","World","Fruits","Vegetables","Antartic","Ameliorate",
        "Finding","Dream","Developer","Particular","Searching","Individual"]
 
-guessing_game=random.choice(words)
+guessing_game=random.choice(words).upper()
 while True:
-  guesing_word=input("Write the guessing word")
+  guesing_word=input("Write the guessing word").strip().upper()
 
   if guesing_word==guessing_game:
     print("Congratulation, your guessing word is correct")
