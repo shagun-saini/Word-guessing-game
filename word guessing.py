20d3cfc01e8e8ea35db48823075c15d1c1b9f0d8
shagun-saini <shagunsaini205@gmail.com>
2026-01-04 23:31:44 +0530

    Fixed logic

--- word guessing.py
+++ word guessing.py
@@ -1,14 +1,16 @@
 #Word guessing game project.
 import random
+while True:
 
-words=["Articles","World","Fruits","Vegetables","Antartic","Ameliorate",
+ words=["Articles","World","Fruits","Vegetables","Antartic","Ameliorate",
        "Finding","Dream","Developer","Particular","Searching","Individual"]
 
-guessing_game=random.choice(words)
+ guessing_game=random.choice(words)
 
-guesing_word=input("Write the guessing word")
+ guesing_word=input("Write the guessing word")
 
-if(guessing_game==words):
+ if(guessing_game==words):
     print("Congratulation, your guessing word is correct")
-else:
+    break
+ else:
     print("Sorry")
